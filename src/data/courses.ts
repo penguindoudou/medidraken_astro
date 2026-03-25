9468c95419f5abc080495f6a781a8b0ac668cfa6
Simon <simonsaysautomate@gmail.com>
2026-03-25 18:15:54 +0100

    adapted the line widths for lighening calender to better match the course pages

---
+++ src/data/courses.ts
@@ -0,0 +1,43 @@
+export type Course = {
+  title: string;
+  /** Vilken typ av kurs det är, t.ex. "Helgkurs" eller "Terminskurs". Visas ex. på index-sidan. */
+  format: string;
+  location: string;
+  date: string;
+  times: string;
+  link: string;
+  /** Denna datumsträng används för att avgöra om kursen är i framtiden (format: YYYY-MM-DD). */
+  lastDate: string;
+};
+
+// ==========================================
+// Medicinsk Qigong
+// ==========================================
+export const coursesQigong: Course[] = [
+  // Exempel på en framtida kurs (Helgkurs-format används nu överallt):
+  //{
+  //  title: 'Grundkurs',
+  //  format: 'Helgkurs',
+  //  location: 'Nyköping',
+  //  date: '14-15 Okt',
+  //  times: '10:00 - 15:00',
+  //  link: '/boka',
+  //  lastDate: '2026-10-15',
+  //},
+];
+
+// ==========================================
+// Tai Chi
+// ==========================================
+export const coursesTaiChi: Course[] = [
+  {
+    title: 'Grundkurs',
+    format: 'Helgkurs',
+    location: 'Nyköping',
+    date: '14-15 Okt',
+    times: '10:00 - 15:00',
+    link: '/boka',
+    lastDate: '2026-03-24',
+  },
+
+];
