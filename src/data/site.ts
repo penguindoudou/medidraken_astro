6864a408b31c975b5f8d0e974d6bf109a8ca0db6
Simon <simonsaysautomate@gmail.com>
2026-03-25 16:15:32 +0100

    centralised adress and website domain

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,8 +1,38 @@
+const domain = 'medidraken.com';
+
 export const siteConfig = {
   name: 'Medidraken',
+  domain,
+  url: `https://www.${domain}`,
   phone: {
     display: '+46 70-581 40 86',
     clean: '+46705814086'
   },
-  email: 'info@medidraken.com',
+  email: `info@${domain}`,
+  addresses: {
+    gnesta: {
+      street: 'Dalgatan 20',
+      zip: '646 32',
+      city: 'Gnesta',
+      full: 'Dalgatan 20, 646 32 Gnesta',
+      mapUrl: 'https://www.google.com/maps/place/Dalgatan+20,+646+32+Gnesta',
+      embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2042.8687258380292!2d17.3056093!3d59.0494875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f2425cf15baff%3A0xcfd6ad4ee2fcefd2!2sDalgatan%2020%2C%20646%2032%20Gnesta!5e0!3m2!1ssv!2sse!4v1700000000000!5m2!1ssv!2sse'
+    },
+    nykoping: {
+      street: 'Östra Storgatan 2',
+      zip: '611 34',
+      city: 'Nyköping',
+      full: 'Östra Storgatan 2, 611 34 Nyköping',
+      mapUrl: 'https://www.google.com/maps/place/Östra+Storgatan+2,+611+34+Nyköping',
+      embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2060.590161471378!2d17.0006764!3d58.7533088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465ed6ca2df44eb1%3A0xcbecccdd15317781!2s%C3%96stra%20Storgatan%202%2C%20611%2034%20Nyk%C3%B6ping!5e0!3m2!1ssv!2sse!4v1700000000000!5m2!1ssv!2sse'
+    },
+    oxelosund: {
+      street: 'Föreningsgatan 30',
+      zip: '613 31',
+      city: 'Oxelösund',
+      full: 'Föreningsgatan 30, 613 31 Oxelösund',
+      mapUrl: 'https://www.google.com/maps/place/Föreningsgatan+30,+613+31+Oxelösund',
+      embedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2066.8284534722883!2d17.0984183!3d58.6698654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4658d55acbb92deb%3A0xe10deba15e378e0e!2sF%C3%B6reningsgatan%2030%2C%20613%2031%20Oxel%C3%B6sund!5e0!3m2!1ssv!2sse!4v1700000000000!5m2!1ssv!2sse'
+    }
+  }
 };
