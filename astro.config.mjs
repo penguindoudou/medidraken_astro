734d83c74967bb0f12a5383151ff931cc0a01b68
Simon <simonsaysautomate@gmail.com>
2026-04-27 02:17:22 +0200

    contact form is working on kontakt.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
     server: {
